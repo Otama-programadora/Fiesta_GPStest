ba7e5bae408bad05638cd616ecf7555d47b7e065
Tamaki <tamaki.kawagoshi@xecstars.co.jp>
2021-06-24 11:05:56 +0900

    Upload GPS info to S3 Changed the info from String into Int(lat, lng)

--- app/src/main/java/com/example/fiesta_gps_done/S3_test.kt
+++ app/src/main/java/com/example/fiesta_gps_done/S3_test.kt
@@ -8,14 +8,18 @@ import android.os.Bundle
 import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.amazonaws.auth.AWSCredentials
+import com.amazonaws.auth.AWSSessionCredentials
 import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.auth.CognitoCachingCredentialsProvider
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility
+import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3Client
 import kotlinx.android.synthetic.main.activity_s3_test.*
 import java.io.BufferedWriter
 import java.io.File
 import java.io.FileWriter
 import java.io.PrintWriter
-import java.lang.Exception
 import java.nio.file.Files
 import java.nio.file.Paths
 import kotlin.concurrent.thread
@@ -23,10 +27,7 @@ import kotlin.concurrent.thread
 
 @Suppress("DEPRECATION")
 class S3_test :AppCompatActivity() {
-    //private val BUCKET:String =
-    //private val ACCESS_KEY:String =
-    //private val SECRET_ACCESS_KEY:String =
-    private val TEST_FILE: String = "test.txt" //S3に上げるファイルの名前
+    // この辺の個人情報を載せたままコミットしない！ Nunca cometas el mismo error; Ten muchísimo cuidado para no mostrar estas llaves a GitHub
 
             /*private val mContext:Context?
             init {
@@ -40,40 +41,46 @@ class S3_test :AppCompatActivity() {
         setContentView(R.layout.activity_s3_test)
 
         //MainActivityから受け取ったデータ（緯度経度）を画面に表示する
-        val GPSdata_lat = intent.getDoubleExtra("latData", 0.0) //デフォルト値って何？
-        val GPSdata_lng = intent.getDoubleExtra("lngData",0.0)
+        val gpsData_lat = intent.getDoubleExtra("latData", 0.0) //デフォルト値って何？
+        val gpsData_lng = intent.getDoubleExtra("lngData",0.0)
 
         //GPSview.text = "緯度は${GPSdata_lat} で、 経度は${GPSdata_lng}"
 
         //MainActivityから受け取ったデータは浮動小数点型なので文字列型にキャストして次の関数に渡す
-        val GPSdata_lat_str:String = GPSdata_lat.toString()
-        val GPSdata_lng_str:String = GPSdata_lng.toString()
+        val gpsData_lat_str:String = gpsData_lat.toString()
+        val gpsData_lng_str:String = gpsData_lng.toString()
 
-        doInBackground(GPSdata_lat_str, GPSdata_lng_str)
+        doInBackground(gpsData_lat_str, gpsData_lng_str)
     }
+
+
 
     @RequiresApi(Build.VERSION_CODES.O)
     //ファイルに位置情報を書いてs3に上げる関数
     fun doInBackground(lat: String, lng: String): Int {
         //Amazon Cognito 認証情報プロバイダーを初期化
-        /*val credentialsProvider = CognitoCachingCredentialsProvider(
+        TODO("ログイン画面をアプリに出すにはどうしたらいい？")
+        val credentialsProvider = CognitoCachingCredentialsProvider(
                 applicationContext,
-                "ap-northeast-1:de70fba0-c8f9-44ec-8a81-7632762dc4b7", // ID プールの ID
+                "ap-northeast-1:1990f673-d620-426c-82e8-2f11cdd2477a", // ID プールの ID
                 Regions.AP_NORTHEAST_1
         )
-        val cred: AWSCredentials = BasicAWSCredentials(ACCESS_KEY2, SECRET_ACCESS_KEY2)
-        val s3: AmazonS3 = AmazonS3Client(cred)*/
+
+        //val endpoint = "https://test-android-programming-27-de-abril.ap-northeast-1.amazonaws.com"
+        //val client = AmazonKinesisClient(cognitoProvider)
+        //client.setEndpoint(endpoint)
 
         try {
             // クライアントを生成
-            var basicAWSCredentials =  BasicAWSCredentials(ACCESS_KEY,SECRET_ACCESS_KEY)
-            var s3 =  AmazonS3Client(basicAWSCredentials)
+            val basicAWSCredentials =  BasicAWSCredentials(ACCESS_KEY,SECRET_ACCESS_KEY)
+            val s3 =  AmazonS3Client(basicAWSCredentials)
 
             val file = File(this.cacheDir, TEST_FILE)
             //File()で新規ファイル作成, ただし空のファイルは送れない //cacheDir:ファイル書き込みできるディレクトリに移動する
             FileWriter(file).use { fw ->
                 BufferedWriter(fw).use { bw ->
                     PrintWriter(bw).use { pw ->
+
                         pw.println(lat)
                         pw.println(lng) // 位置情報を書き込み
                     }
@@ -84,10 +91,10 @@ class S3_test :AppCompatActivity() {
 
             //ネットワークとのやりとりはメインスレッドではできない！
             thread {
-                //val transferUtility = TransferUtility.builder().s3Client(s3).context(applicationContext).build()
+                val transferUtility = TransferUtility.builder().s3Client(s3).context(applicationContext).build()
 
-                //Files.createFile(Paths.get(TEST_FILE))
-                //val transferObserver = transferUtility.upload(BUCKET, "test.txt", file)
+                Files.createFile(Paths.get(TEST_FILE))
+                val transferObserver = transferUtility.upload(BUCKET, "test.txt", file)
                 s3.putObject(BUCKET, TEST_FILE, file)
 
                 val result = s3.listObjects(BUCKET)
